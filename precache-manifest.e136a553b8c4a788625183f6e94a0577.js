049f5857efde8a3c6fee66557722919862682372
Максим Пешехонов <max.peshehonov@yandex.ru>
2020-05-03 19:55:29 +0300

    Updates

---
+++ precache-manifest.e136a553b8c4a788625183f6e94a0577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bd418413c13ada6201d87865c69d82",
+    "url": "/virus-hack/index.html"
+  },
+  {
+    "revision": "f9ecc7b61f86706e2443",
+    "url": "/virus-hack/static/css/main.4c454f71.chunk.css"
+  },
+  {
+    "revision": "2f691d5cf1eaecac9e14",
+    "url": "/virus-hack/static/js/2.72cecd5a.chunk.js"
+  },
+  {
+    "revision": "ccfb27d768df50df1edb1a2bb04b68be",
+    "url": "/virus-hack/static/js/2.72cecd5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ecc7b61f86706e2443",
+    "url": "/virus-hack/static/js/main.9b130405.chunk.js"
+  },
+  {
+    "revision": "9b2802ddec29fb78a0a6",
+    "url": "/virus-hack/static/js/runtime-main.7a8f9d80.js"
+  }
+]);
