36b3d878e0fffa8181c9b875945eb6ea22f7787f
Максим Пешехонов <max.peshehonov@yandex.ru>
2020-05-04 19:30:28 +0300

    мед карта

--- src/blocks/HealthMetrics/HealthMetrics.js
+++ src/blocks/HealthMetrics/HealthMetrics.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './HealthMetrics.scss';
 import {Space} from "antd";
 
--- src/pages/PatientCardPage/PatientCardPage.js
+++ src/pages/PatientCardPage/PatientCardPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './PatientCardPage.scss';
 import {Layout} from "antd";
-import HealthMetrics from '../../blocks/HealthMetrics';
-import Recommends from '../../blocks/Recommends';
-import Drugs from '../../blocks/Drugs';
-import News from '../../blocks/News';
-import Education from '../../blocks/Education';
 import MobileMenu from "../../blocks/MobileMenu";
-import PatientActions from "../../blocks/PatientActions";
+import MetricsWidget from "../../blocks/MetricsWidget";
+import MetricsGraphWidget from "../../blocks/MetricsGraphWidget";
+import DynamicWidget from "../../blocks/DynamicWidget";
+import Records from "../../blocks/Records/Records";
+import HealingCourse from "../../blocks/HealingCourse";
+import RecommendsForecast from "../../blocks/RecommendsForecast";
 
 const PatientCardPage = () => {
     return (
@@ -18,14 +18,14 @@ const PatientCardPage = () => {
                         <path d="M0 7.5H18V10.5H0V7.5ZM0 0H24V3H0V0ZM0 18H10.8525V15H0V18Z" fill="white"/>
                     </svg>
                     <h1>Медкарта</h1>
-                    <PatientActions/>
+                    <MetricsWidget/>
                 </div>
                 <Layout.Content className="layout-content site-layout-background layout-br-top">
-                    <HealthMetrics />
-                    <Recommends />
-                    <Drugs />
-                    <News/>
-                    <Education/>
+                    <MetricsGraphWidget />
+                    <DynamicWidget/>
+                    <Records/>
+                    <HealingCourse/>
+                    <RecommendsForecast/>
                 </Layout.Content>
             </Layout>
             <MobileMenu/>
